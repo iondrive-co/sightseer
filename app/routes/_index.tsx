1bac6081c0ce7e13ef3a5c2946f30e23a8a862ce
pid <pid@iondrive.co>
2023-05-26 23:18:01 +1000

    Add css for a sidebar + main panel content button

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,23 +1,32 @@
-import type { V2_MetaFunction } from "@remix-run/cloudflare";
 import { Link } from "@remix-run/react";
+import { useState } from "react";
+import "../styles/styles.css";
 
 export const meta: V2_MetaFunction = () => {
   return [
     { title: "Demo App" },
-    { name: "description", content: "Welcome to Ion Drive" },
+    { name: "description", content: "Welcome" },
   ];
 };
 
 export default function Index() {
+  const [content, setContent] = useState(''); // Initialize state
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <div className="mx-auto mt-16 max-w-7xl text-center">
-        <Link
-          to="/links"
-          className="text-xl text-blue-600 underline"
-        >
+    <div className="app">
+      <div className="sidebar">
+        <Link to="/links" className="text-xl text-blue-600 underline">
           Links
         </Link>
+        <button 
+          onClick={() => setContent('These are some references.')}
+          className="text-xl text-blue-600 underline cursor-pointer"
+        >
+          References
+        </button>
+      </div>
+      <div className="main-content">
+        {/* main content will change based on state */}
+        <p>{content}</p>
       </div>
     </div>
   );
--- remix.env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="@remix-run/dev" />
-/// <reference types="@remix-run/cloudflare" />
-/// <reference types="@remix-run/node" />
-/// <reference types="@cloudflare/workers-types" />
