df6ae78d9d926e67fc343a890336f9d238ef528c
Miles <miles@iondrive.co>
2023-07-17 23:49:40 +1000

    Better wrapping on mobile

--- app/components/ExobaseLoader.ts
+++ app/components/ExobaseLoader.ts
@@ -10,11 +10,27 @@ export const exobaseLoader: LoaderFunction = async ({ params }) => {
             throw new Error('Page not found');
         }
 
-        const parsedContent = content.replace(/\[\[(.*?)\]\]/g, function (match, p1) {
+        const lines = content.split(/\n|\r\n/);
+        const processedLines = lines.map(line => {
+            if (line.startsWith("- ")) {
+                // This is a list item
+                return `<li>${line.substring(2)}</li>`;  // Remove the "- " and wrap in <li>
+            } else if (line.trim() === '') {
+                // This is a blank line, treat it as a paragraph separator
+                return '</ul><p></p><ul>';
+            } else {
+                // This is a regular line
+                return line;
+            }
+        });
+        const processedContent = processedLines.join(' ');
+        const finalContent = processedContent.replace(/\[\[(.*?)\]\]/g, function (match, p1) {
             return `<a href="/exobase/${p1}">${p1}</a>`;
         });
 
-        return json({ content: parsedContent });
+        const finalHtml = `<ul>${finalContent}</ul>`;
+
+        return json({ content: finalHtml });
     } catch (error) {
         return json({ error: (error as any).message }, { status: 404 });
     }
