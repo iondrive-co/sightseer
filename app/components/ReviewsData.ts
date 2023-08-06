6253d13d2ee6b87e014147fdeab40b430aa51dac
Miles <miles@iondrive.co>
2023-08-06 18:22:19 +1000

    Add ratings to each movie and sort by rating

--- app/components/ReviewsData.ts
+++ app/components/ReviewsData.ts
@@ -2,6 +2,10 @@ export interface Movie {
     title: string;
     thumbnail: string;
     summary: string;
+    e: number;
+    a: number;
+    n: number;
+    c: number;
 }
 
 export const sciFiMoviesData: Movie[] = [
@@ -9,90 +13,146 @@ export const sciFiMoviesData: Movie[] = [
         title: "Alien",
         thumbnail: "https://upload.wikimedia.org/wikipedia/en/c/c3/Alien_movie_poster.jpg",
         summary: `Ridley Scott's slow-burning, atmospheric filmmaking melds a first-rate screenplay, Giger's art, and
-        Sigourney Weaver's formidable performance into a masterpiece of sci-fi cinema`
+        Sigourney Weaver's formidable performance into a masterpiece of sci-fi cinema`,
+        e: 8,
+        a: 10,
+        n: 10,
+        c: 7,
     },
     {
         title: "Aliens",
         thumbnail: "https://upload.wikimedia.org/wikipedia/en/f/fb/Aliens_poster.jpg",
         summary: `A textbook sequel which takes the essence of the first and pivots to a larger action movie while 
-        keeping the atmosphere, suspense, and strong lead of the original`
+        keeping the atmosphere, suspense, and strong lead of the original`,
+        e: 8,
+        a: 9,
+        n: 9,
+        c: 7,
     },
     {
         title: "Animatrix, The",
         thumbnail: "https://static.wikia.nocookie.net/toonami/images/8/82/Animatrix.jpg",
         summary: `Directed by some notable animators, The Animatrix was a slick way to add flavour to the Matrix 
-        universe via an atmospheric series of short films, which ended up being more enjoyable than the subsequent movies`
+        universe via an atmospheric series of short films, which ended up being more enjoyable than the subsequent movies`,
+        e: 8,
+        a: 9,
+        n: 10,
+        c: 6,
     },
     {
         title: "Donnie Darko",
         thumbnail: "https://www.themoviedb.org/t/p/w1280/3PiQIQzJX2oE8Uw6gjL08BXQRoB.jpg",
         summary: `A weird, soulful film that peels back an outer layer of teenage angst and superficial 80s suburbia to 
-        delve into mental illness, quantum physics and time travel, and tie them together into a beautiful piece of art`
+        delve into mental illness, quantum physics and time travel, and tie them together into a beautiful piece of art`,
+        e: 9,
+        a: 7,
+        n: 10,
+        c: 8,
     },
     {
         title: "Ex Machina",
         thumbnail: "https://upload.wikimedia.org/wikipedia/en/b/ba/Ex-machina-uk-poster.jpg",
         summary: `An excellently paced sci-fi thriller, we are driven through a beautifully atmospheric setting by an 
         extremely compelling idea to the inevitable conclusion, with the conflicting personalities and strength of the
-        ideas making this a completely enthralling journey from start to finish.`
+        ideas making this a completely enthralling journey from start to finish.`,
+        e: 8,
+        a: 8,
+        n: 8,
+        c: 8,
     },
     {
         title: "Gravity",
         thumbnail: "https://www.themoviedb.org/t/p/w1280/krQrQSNb0EXd5L0e2CkoU5V6XNq.jpg",
         summary: `Watching this sci-fi thriller in IMAX was wild, this movie is a completely engrossing white knuckle 
         ride with stunning SFX vistas and a simple compelling plot that is exceptionally well executed by both main 
-        characters`
+        characters`,
+        e: 7,
+        a: 10,
+        n: 8,
+        c: 8,
     },
     {
         title: "Inception",
         thumbnail: "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
         summary: `A cultural phenomenon that overcomes its absurdity and weak characters with blockbuster effects
-        and a few intriguing plot hooks, much like the Matrix 11 years earlier`
+        and a few intriguing plot hooks, much like the Matrix 11 years earlier`,
+        e: 8,
+        a: 9,
+        n: 8,
+        c: 7,
     },
     {
         title: "Jurassic Park",
         thumbnail: "https://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg",
         summary: `Michael Crichton’s page turner was the perfect Spielberg blockbuster, best watched as a small child
-        but really timeless action packed sci-fi for all ages, with a few darker warnings remaining from the novel`
+        but really timeless action packed sci-fi for all ages, with a few darker warnings remaining from the novel`,
+        e: 9,
+        a: 9,
+        n: 9,
+        c: 6,
     },
     {
         title: "Martian, The",
         thumbnail: "https://upload.wikimedia.org/wikipedia/en/c/cd/The_Martian_film_poster.jpg",
         summary: `Andy Weir's upbeat, funny protagonist of science Mark Watney is a perfect fit for Matt Damon, and
-         coupled with Ridley Scott's eye for the stunning visuals this is a glorious and very enjoyable movie`
+         coupled with Ridley Scott's eye for the stunning visuals this is a glorious and very enjoyable movie`,
+        e: 8,
+        a: 8,
+        n: 8,
+        c: 8,
     },
     {
         title: "Matrix, The",
         thumbnail: "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
         summary: `The Matrix was a groundbreaking and visually stunning sci-fi that survives multiple rewatchings
-        due to the tight plot, pop philosophy, and fun action sequences leading up to a satisfying conclusion`
+        due to the tight plot, pop philosophy, and fun action sequences leading up to a satisfying conclusion`,
+        e: 9,
+        a: 8,
+        n: 9,
+        c: 6,
     },
     {
         title: "Monsters",
         thumbnail: "https://upload.wikimedia.org/wikipedia/en/3/35/MonstersUK.jpg",
         summary: `Jon Hopkins delicate, beautiful score is the perfect accompaniment to this sci-fi romance, and along 
         with the top notch cinematography it brings to life the wonder of the strange alien life in the film's 
-        believable devastated landscape, despite the shoestring budget this was shot on`
+        believable devastated landscape, despite the shoestring budget this was shot on`,
+        e: 8,
+        a: 9,
+        n: 8,
+        c: 7,
     },
     {
         title: "Moon",
         thumbnail: "https://upload.wikimedia.org/wikipedia/en/a/af/Moon_%282009_film%29.jpg",
         summary: `This minimalistic gem uses a haunting score and few slick effect shots of the 
         indifferent beauty of the moon to perfectly set up an atmosphere of claustrophobic isolation, 
-        into which Sam Rockwell delivers a riveting performance through some tightly written plot beats`
+        into which Sam Rockwell delivers a riveting performance through some tightly written plot beats`,
+        e: 8,
+        a: 10,
+        n: 8,
+        c: 7,
     },
     {
         title: "Solaris (2002)",
         thumbnail: "https://upload.wikimedia.org/wikipedia/en/b/bf/Solaris2002poster.jpg",
         summary: `Soderbergh's adaptation is more approachable and less cerebral than Tarkovsky's due to his focus
         on the character relationships rather than the larger metaphysical concerns of the novel, and this angle 
-        ultimately yields an enjoyable and well told tale`
+        ultimately yields an enjoyable and well told tale`,
+        e: 9,
+        a: 8,
+        n: 8,
+        c: 8,
     },
     {
         title: "War Of The Worlds (2005)",
         thumbnail: "https://upload.wikimedia.org/wikipedia/en/8/83/War_of_the_Worlds_2005_poster.jpg",
         summary: `There is something very cathartic about the deadbeat dad redemption and post 9-11 trauma themes
         worked throughout, and along with the effective build up of tension and amazing special effects, that makes 
-        this an extremely enjoyable movie`
+        this an extremely enjoyable movie`,
+        e: 7,
+        a: 9,
+        n: 9,
+        c: 8,
     },
 ];
--- app/routes/Reviews._index.tsx
+++ app/routes/Reviews._index.tsx
@@ -13,15 +13,15 @@ export default function Reviews() {
     let movies: Movie[] = useLoaderData();
 
     let movieGroups = movies.reduce((groups, movie) => {
-        let firstLetter = movie.title[0].toUpperCase();
-        if (!groups[firstLetter]) {
-            groups[firstLetter] = [];
+        let totalRating = movie.e + movie.a + movie.n + movie.c;
+        if (!groups[totalRating]) {
+            groups[totalRating] = [];
         }
-        groups[firstLetter].push(movie);
+        groups[totalRating].push(movie);
         return groups;
-    }, {} as { [key: string]: Movie[] });
+    }, {} as { [key: number]: Movie[] });
 
-    let sortedGroupKeys = Object.keys(movieGroups).sort();
+    let sortedGroupKeys = Object.keys(movieGroups).sort((a, b) => parseInt(b) - parseInt(a));
 
     return (
         <div className="app">
@@ -29,19 +29,26 @@ export default function Reviews() {
             <div className="main-container">
                 <div className="description-container">
                     <p>
-                        Enjoyable sci-fi movies, mouse over each for a no-spoiler one line review.
+                    Enjoyable sci-fi movies, mouse over each for a no-spoiler one line review. Rating is out of 40 based
+                    on 4 equal categories, (e)xploration of premise, (a)tmosphere, (n)arrative structure, (c)haracterisation
                     </p>
                 </div>
                 <div className="container">
-                    {sortedGroupKeys.map(groupKey => (
+                    {sortedGroupKeys.map((groupKey: any) => (
                         <div key={groupKey}>
                             <h1>{groupKey}</h1>
-                            {movieGroups[groupKey].map(movie => (
+                            {movieGroups[+groupKey].map(movie => (
                                 <div key={movie.title} className="movie">
                                     <img src={movie.thumbnail} alt={movie.title} />
                                     <div className="content">
                                         <h2>{movie.title}</h2>
                                         <p>{movie.summary}</p>
+                                        <div className="rating">
+                                            <span className="rating-item">e: {movie.e}</span>
+                                            <span className="rating-item">a: {movie.a}</span>
+                                            <span className="rating-item">n: {movie.n}</span>
+                                            <span className="rating-item">c: {movie.c}</span>
+                                        </div>
                                     </div>
                                 </div>
                             ))}
