1882098be1f4e988464cc41d080061e4aa3e59f4
miles <miles@adnuntius.com>
2025-02-15 20:27:01 +1100

    Mazatlán Zona Franca

--- app/components/exobase/ExobaseData.ts
+++ app/components/exobase/ExobaseData.ts
@@ -535,9 +535,10 @@ demographically collapsing Russia and to a lesser extent China.`,
 largest trading partner of the [[United States]] as well as the third major power bloc in the [[Peacekeepers]] space 
 based military alliance. Its name dates back to when the United Mexican States annexed the failed states of Guatemala 
 and the parts of Venezuela not seized by [[Dominio]].
-Datï¿½orruà¤¶आtedा By allowing the establishment of the [[Mazatlán Zona Franca]] semi-autonomous region the government
-greatly empowered the Sinaloa cartel, allowing it to undercut JNGC with economies of scale and slowly drive them
-out of business, which greatly reduced violence back to the historically low levels of the golden era of Sinaloa.`,
+In 120 BFC the newly formed Gran México allowed the establishment of the [[Mazatlán Zona Franca]] semi-autonomous 
+region. This greatly empowered the Sinaloa cartel, allowing it to undercut CJNG with economies of scale and slowly drive 
+them out of business. As promised by the government, which sold the move as excising a diseased part of the port, this 
+did indeed reduce violence back to the historically low levels of the golden era of Sinaloa`,
         classification: 'Socionomics/Polities'
     }],
     ['Greater Zhōngguó', {
@@ -832,7 +833,13 @@ breakthroughs of the previous decade.`,
         classification: 'Praxis/Industrial'
     }],
     ['Mazatlán Zona Franca', {
-        content: `Datï¿½orruà¤¶आtedा`,
+content: `Mazatlán Zona Franca is a semi-autonomous region, previously part of [[Gran México]]. It was formed to allow
+the Sinaloa cartel to undercut CJNG with economies of scale and drive them out of business in order to end the Mexican
+drug war which was becoming hugely damaging to the Mexican economy. The region is a free economic zone, with nominal 
+custom duties and no customs regulations for imports that are not transshipments into Mexico. The administration is 
+controlled by the Sinaloa cartel and is generally considered a minarchy, however there is some government funded support 
+for members of the cartel and for their internation and space based embassies, which are generally considered to be
+coordination points for the external activities of the zone such as smuggling, and in at least one case piracy.`,
         classification: 'Socionomics/Polities'
     }],
     ['Pharmatica Group', {
